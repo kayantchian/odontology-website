fdda8c1882efbd718a05dadb41620bc60455a020
kayan <paranormalt7@gmail.com>
2022-09-16 01:11:57 -0300

    primeiro commit

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<!-- Contato rápido -->
+<div class="py-1 bg-black top">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
+	    		<div class="col-lg-12 d-block">
+		    		<div class="row d-flex">
+		    			<div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">+ 3257 2356</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">odontofortaleza@gmail.com</span>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+	<!-- Barra de navegação -->
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" style="align:left" href="index.php"><img src="images/logo.png" width="600" height="250"></a>
+	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav nav ml-auto">
+	          <li class="nav-item"><a href="#home-section" class="nav-link"><span>Início</span></a></li>
+	          <li class="nav-item"><a href="#sobre-section" class="nav-link"><span>Sobre</span></a></li>
+	          <li class="nav-item"><a href="#servicos-section" class="nav-link"><span>Serviços</span></a></li>
+	          <li class="nav-item"><a href="#doutores-section" class="nav-link"><span>Doutores</span></a></li>
+	          <li class="nav-item"><a href="#contato-section" class="nav-link"><span>Contato</span></a></li>
+	          <li class="nav-item cta mr-md-1"><a href="appointment.html" class="nav-link">Consulta</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
