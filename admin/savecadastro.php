d27a96079a99163a74b11ef935a9a3193c63671d
Kayan Tchian <44986550+kayantchian@users.noreply.github.com>
2022-09-19 11:27:58 -0300

    correção "savecadastro.php" #3

--- admin/savecadastro.php
+++ admin/savecadastro.php
@@ -3,20 +3,25 @@ session_start();
 include 'protect.php';
 include '../sql/conexao.php';
 
-$nome = $_POST['nome'];
-$cpf = $_POST['cpf'];
-$telefone = $_POST['telefone'];
-$servico = $_POST['servico'];
-$data = $_POST['data'];
-$horario = $_POST['horario'];
+f(isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['cpf']) && !empty($_POST['telefone']) && !empty($_POST['servico']) && !empty($_POST['data']) && !empty($_POST['horario'])){
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $telefone = $_POST['telefone'];
+    $servico = $_POST['servico'];
+    $data = $_POST['data'];
+    $horario = $_POST['horario'];
 
 
-$inserir = $con -> query ("INSERT INTO clientes VALUES ('0', '$nome', '$cpf', '$telefone', '$servico','$data','$horario')");
-    if ($inserir) {
-        header("location: cadastrar.php");
-        echo '<script>alert("Cadastro Efetuado!");</script>';
-    } else {
-        echo '<script>alert("NÃO FOI POSSÍVEL REALIZAR CADASTRO!");</script>';
-        header("location: painel.php");
+    $inserir = $con -> query ("INSERT INTO clientes VALUES ('0', '$nome', '$cpf', '$telefone', '$servico','$data','$horario')");
+        if ($inserir) {
+            header("location: cadastrar.php");
+            echo '<script>alert("Cadastro Efetuado!");</script>';
+        } else {
+            echo '<script>alert("NÃO FOI POSSÍVEL REALIZAR CADASTRO!");</script>';
+            header("location: painel.php");
+        }
     }
+    else {
+        header("location:painel.php");
+      }
 ?>
